929e1f5f3cd90b2b5a344cf8dad17ee9eb60d3f5
Jag Talon <jag@jagtalon.com>
2020-01-09 09:06:52 -0500

    Move from https://github.com/jagtalon/figma-plugins

---
+++ src/code.ts
@@ -0,0 +1,29 @@
+async function loadFont(node) {
+  // console.log(node.fontName);
+  await figma.loadFontAsync(node.fontName);
+}
+
+// Show plugin UI.
+figma.showUI(__html__);
+figma.ui.resize(400, 220);
+
+// Fetch all the text in the selected frame.
+// [<Name of the object>, <Text contents>]
+let selection: ReadonlyArray<SceneNode> = figma.currentPage.selection;
+let textFound: Array<Array<string>> = [];
+
+selection.forEach(selected => {
+  let textNodes = (selected as any).findAll(node => node.type === "TEXT");
+  textNodes.forEach(node => { 
+    textFound.push([node.name, node.characters]);
+  });
+});
+
+// Send the text that we got from the frame to the UI.
+figma.ui.postMessage(textFound);
+
+figma.ui.onmessage = msg => {
+ if(msg.type === 'notification') {
+   figma.notify(msg.message, {timeout: 1});
+ }
+}
---
+++ src/ui.ts
@@ -0,0 +1,49 @@
+// Import Figma styles
+import './libs/figma-ds/figma-plugin-ds.min.css'
+import './libs/figma-ds/figma-plugin-ds.min.js'
+
+import './ui.css'
+
+// Receive all the text nodes of the selected frame from the plugin.
+onmessage = (event) => {
+  let pluginMessage: string[][] = event.data.pluginMessage,
+      table: HTMLTableElement = document.createElement('table'),
+      container: HTMLElement = document.querySelector('.plugin__container');
+
+  // Attach the table to the container.
+  container.append(table);
+
+  // Build the table.
+  // TODO: Probably best to refactor this into either Vue or React.
+  pluginMessage.forEach(list => {
+    let tr: HTMLTableRowElement = document.createElement('tr');
+    table.append(tr);
+
+    // Map the list of strings into a list of HTMLTableCellElements.
+    list.map(text => {
+      let td: HTMLTableCellElement = document.createElement('td');
+      td.append(document.createTextNode(text));
+      td.setAttribute('contenteditable', 'true');
+      td.onclick = copyContent;
+
+      return td;
+    }).forEach(cell => {
+      tr.append(cell);
+    });
+  });
+}
+
+// From https://stackoverflow.com/a/6150060
+function copyContent(event: MouseEvent) {
+  var range: Range = document.createRange();
+  range.selectNodeContents(this);
+  
+  var sel: Selection = window.getSelection();
+  sel.removeAllRanges();
+  sel.addRange(range);
+
+  document.execCommand('copy');
+
+  // Tell the plugin about it.
+  parent.postMessage({pluginMessage: {type: 'notification', message: 'Copied Text'}}, '*');
+}
